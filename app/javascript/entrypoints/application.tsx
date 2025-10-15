ea328c0dbc95ebbe9c9966db7d970a9dde2bf4ed
Yoshioka Taichi | 吉岡 大一 <taichocop@gmail.com>
2025-10-15 20:17:45 +0900

    feat: implement HelloWorld component and update entrypoint to render it

--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -1,28 +1,11 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
+import { createRoot } from "react-dom/client";
+import HelloWorld from "@/components/HelloWorld";
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+console.log("Vite ⚡️ Rails ⚡️ Foreman");
 
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  if (root) {
+    createRoot(root).render(<HelloWorld />);
+  }
+});
